5a161beb02875247025b4223ab6bfbaa730e05b3
Geethanjali <geethanjalis5657@gmail.com>
2024-10-10 09:54:47 +0530

    git test

---
+++ FRONTEND/src/pages/user/Home.jsx
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+const HomePage = () => {
+
+
+  return (
+    <div>
+     git test
+    </div>
+  );
+};
+
+export default HomePage;
